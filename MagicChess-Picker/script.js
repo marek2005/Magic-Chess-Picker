016e4de651afe41c0ceaceb4c594f70892c95e9b
Marek Pisetsky <marekpisetsky001@gmail.com>
2023-12-17 13:33:39 +0100

    Codigo Base de la Pagina
    En esta linea de tiempo tengo el codigo por donde puedo empezar a crear la pagina y parte del Header completado

---
+++ MagicChess-Picker/script.js
@@ -0,0 +1,9 @@
+var checkbox = document.getElementById('checkbox');
+var options = document.getElementById('menu');
+
+options.style.display = 'none';
+
+checkbox.addEventListener('change', function() {
+  options.style.display = checkbox.checked ? 'block' : 'none';
+});
+
