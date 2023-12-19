57ed97d3becd38f3c0cc3b0488676a95a5fe15eb
Marek Pisetsky <marekpisetsky001@gmail.com>
2023-12-19 18:16:02 +0100

    Delete MagicChess-Picker directory
    elimino lo anterior para actualizarlo

--- MagicChess-Picker/script.js
+++
@@ -1,9 +0,0 @@
-var checkbox = document.getElementById('checkbox');
-var options = document.getElementById('menu');
-
-options.style.display = 'none';
-
-checkbox.addEventListener('change', function() {
-  options.style.display = checkbox.checked ? 'block' : 'none';
-});
-
